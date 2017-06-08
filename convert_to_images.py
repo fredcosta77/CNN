a525ce715cd93430d6c5b0f2605e1fdd935618e1
Justin Whitaker <justindanielwhitaker@gmail.com>
2017-06-08 11:19:51 -0400

    Keep color

--- convert_to_images.py
+++ convert_to_images.py
@@ -7,7 +7,7 @@ for i in [("input", (28,28)), ("conv1", ()), ("conv2", ()), ("fc", ()), ("output
 		fname = i[0].replace('.pickle','')
 		f = open(fname + ".pickle", "rb")
 		data = pickle.load(f)
-		plt.imshow(np.reshape(data,i[1]), interpolation="nearest", cmap="gray")
+		plt.imshow(np.reshape(data,i[1]))
 		plt.savefig(fname + ".png")	
 	except:
 		pass
